9603c842323bf8d77d5417dc14d60720532613f4
alabamaster1337 <alabamaster1@gmail.com>
2019-02-07 23:20:45 +0300

    Update search.php

--- search.php
+++ search.php
@@ -9,11 +9,12 @@ $gi = geoip_open('inc/gip/GeoIP.dat', GEOIP_STANDARD );
 $search = trim($_GET['search']);
 
 if ( !empty($search) ) {
-	$query = $pdo->query("SELECT * FROM amx_bans WHERE player_nick LIKE '%$search%' OR player_id LIKE '%$search%' OR player_ip LIKE '%$search%' ORDER BY bid ASC");
+	$sql = "SELECT * FROM amx_bans WHERE player_nick LIKE :search OR player_id LIKE :search OR player_ip LIKE :search ORDER BY bid ASC";
+	$params = [ ':search' => $search ];
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute($params);
 }
-
-$tot = $query->rowCount();
-if( $tot > 0 ){ ?>
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -24,26 +25,15 @@ if( $tot > 0 ){ ?>
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<!-- Popper JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
 	<!-- Font Awesome Icons -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/64ff6e1601.css">
-
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
-
 	<!-- My CSS -->
 	<link rel="stylesheet" href="styles.css">
 </head>
-
 <body>
 	<div class="container">
 		<div style="padding-bottom: 60px;"><?php require_once 'menu.php';?></div>
@@ -51,7 +41,6 @@ if( $tot > 0 ){ ?>
 			<div class="col">
 				<div class="card">
 					<div class="card-body" style="font-size: 13px;">
-						<h5 class="text-center">Поиск: <b style="color:blue;"><?=$search;?></b>, совпадений: <b style="color:blue;"><?=$tot;?></b></h5>
 						<table class="table table-hover table-sm table-bordered table-striped">
 							<thead>
 								<tr>
@@ -64,27 +53,32 @@ if( $tot > 0 ){ ?>
 							</thead>
 							<tbody>
 								<?php 
-								while ( $row = $query->fetch(PDO::FETCH_ASSOC) ) { ?>
-									<tr>
-										<td><?php echo '<img src="flags/'.( ( !geoip_country_code_by_addr( $gi, $row['player_ip'] ) ) ? ( 'clear' ) : ( strtolower( geoip_country_code_by_addr( $gi, $row['player_ip'] ) ) ) ).'.png" title="'.(( !geoip_country_name_by_addr( $gi, $row['player_ip'] ) ) ? ( 'Unknown' ) : ( strtolower( geoip_country_name_by_addr( $gi, $row['player_ip'] ) ) ) ).'" />';?> <a href="<?=$url;?>shit.php?id=<?=$row['bid'];?>"><?=$row['player_nick'];?></a></td>
-										<td><?=$row['server_name'];?></td>
-										<td><?=$row['admin_nick'];?></td>
-										<td><?=tm($row['ban_created']);?></td>
-										<td>
-											<?php 
-												if ( $row['ban_length'] == -1 ) {
-													echo '<div style="color: #00ad17;">Разбанен</div>';
-												} elseif ( $row['expired'] == 1 ) {
-													echo '<div style="color: #00ad17;">' . $row['ban_length'] . ' мин.</div>';
-												} elseif ( $row['ban_length'] == 0 ) {
-													echo '<div style="color: #c50000;">Навсегда</div>';
-												} else {
-													echo $row['ban_length'] . ' мин.';
-												}
-											;?>
-										</td>
-									</tr>
-								<?php }?>
+								$tot = $stmt->rowCount();
+								if( $tot > 0 ){
+									while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) { ?>
+										<tr>
+											<td><?php echo '<img src="flags/'.( ( !geoip_country_code_by_addr( $gi, $row['player_ip'] ) ) ? ( 'clear' ) : ( strtolower( geoip_country_code_by_addr( $gi, $row['player_ip'] ) ) ) ).'.png" title="'.(( !geoip_country_name_by_addr( $gi, $row['player_ip'] ) ) ? ( 'Unknown' ) : ( strtolower( geoip_country_name_by_addr( $gi, $row['player_ip'] ) ) ) ).'" />';?> <a href="<?=$url;?>shit.php?id=<?=$row['bid'];?>"><?=$row['player_nick'];?></a></td>
+											<td><?=$row['server_name'];?></td>
+											<td><?=$row['admin_nick'];?></td>
+											<td><?=tm($row['ban_created']);?></td>
+											<td>
+												<?php 
+													if ( $row['ban_length'] == -1 ) {
+														echo '<div style="color: #00ad17;">Разбанен</div>';
+													} elseif ( $row['expired'] == 1 ) {
+														echo '<div style="color: #00ad17;">' . $row['ban_length'] . ' мин.</div>';
+													} elseif ( $row['ban_length'] == 0 ) {
+														echo '<div style="color: #c50000;">Навсегда</div>';
+													} else {
+														echo $row['ban_length'] . ' мин.';
+													}
+												;?>
+											</td>
+										</tr>
+									<?php }?>
+						<?php   } else {
+							echo '<div class="alert alert-danger" role="alert">Ничего не найдено</div>';
+						}?>
 							</tbody>
 						</table>
 					</div>
@@ -94,4 +88,3 @@ if( $tot > 0 ){ ?>
 	</div>
 </body>
 </html>
-<?php } ?>
